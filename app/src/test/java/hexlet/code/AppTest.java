932f34a3b08cd47f88f7e61780a2caaf717c9600
AndrewPorshnev <andrew.porshnev@yandex.ru>
2023-04-12 14:05:49 +0300

    Добавляет класс тестов

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+
+}
